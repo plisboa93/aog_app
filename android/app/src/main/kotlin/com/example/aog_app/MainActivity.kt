2168dd491299058d559c343130cd77d4cdc9c259
Pedro Gabriel <pedrogabriel3@gmail.com>
2020-08-13 03:02:43 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/aog_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aog_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:aog_app/widgets/input.widget.dart';
+import 'package:aog_app/widgets/logo.widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alcool ou Gasolina',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
+  var _alcCtrl = new MoneyMaskedTextController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Logo(),
+          Input(
+            ctrl: _gasCtrl,
+            label: "Gasolina",
+          ),
+          Input(
+            ctrl: _alcCtrl,
+            label: "Álcool",
+          ),
+        ],
+      ),
+    );
+  }
+}
