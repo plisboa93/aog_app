5518d3912d9a63bed7d890fe151685a8033d7883
Pedro Gabriel <pedrogabriel3@gmail.com>
2020-08-14 16:02:06 -0300

    pop up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:aog_app/widgets/input.widget.dart';
+import 'package:aog_app/widgets/loading-button.widget.dart';
 import 'package:aog_app/widgets/logo.widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
@@ -57,27 +58,11 @@ class HomePage extends StatelessWidget {
                 SizedBox(
                   height: 20,
                 ),
-                Container(
-                  margin: EdgeInsets.all(30),
-                  height: 60,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.8),
-                    borderRadius: BorderRadius.circular(
-                      60,
-                    ),
-                  ),
-                  child: FlatButton(
-                    child: Text(
-                      "CALCULAR NOVAMENTE",
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 35,
-                        fontFamily: "Big Shoulders Display",
-                      ),
-                    ),
-                    onPressed: (){},
-                  ),
+                Loadingbutton(
+                  busy: false,
+                  func: (){},
+                  text: "CALCULAR",
+                  invert: true,
                 ),
               ],
             ),
@@ -90,30 +75,15 @@ class HomePage extends StatelessWidget {
             ctrl: _alcCtrl,
             label: "Álcool",
           ),
-          Container(
-            margin: EdgeInsets.all(40),
-            height: 60,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(
-                60,
-              ),
-            ),
-            child: FlatButton(
-              child: Text(
-                "CALCULAR",
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 35,
-                  fontFamily: "Big Shoulders Display",
-                ),
-              ),
-              onPressed: (){},
-            ),
+          Loadingbutton(
+            busy: false,
+            func: (){},
+            text: "CALCULAR",
+            invert: false,
           ),
         ],
-      ),
-    );
+       ),
+      );
   }
 }
 
